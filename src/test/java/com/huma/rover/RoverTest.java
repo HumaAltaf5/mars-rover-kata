ec1d9f2c72272d8ebfd00d1b9410ec4bdae4a5aa
HumaAltaf5 <huma.altaf@gmail.com>
2022-01-25 18:30:53 +0000

    Added test to get current x and y coordinates from input and new class Cordinate with getter and setter for x and y.

--- src/test/java/com/huma/rover/RoverTest.java
+++ src/test/java/com/huma/rover/RoverTest.java
@@ -2,6 +2,9 @@ package com.huma.rover;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 public class RoverTest {
@@ -15,5 +18,16 @@ public class RoverTest {
         // Act and Assert
         plateau.setPlateauSize(10,10);
         assertEquals(expected, plateau.getPlateauSize());
+    }
+
+    @Test
+    public void TestToGetXAndYCoordinate() {
+        //Assert
+        String input1 = "1 2 N";
+        String expected = "1 2";
+
+        //Act and Assert
+        Coordinate coordinate =  new Coordinate(Character.getNumericValue(input1.charAt(0)), Character.getNumericValue(input1.charAt(2)));
+        assertEquals(coordinate.getX() + " " + coordinate.getY(), expected);
     }
 }
